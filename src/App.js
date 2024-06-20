601f38673050f90da7966e64a5e962f47cbb2c93
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-21 01:51:18 +0700

    list products

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Product from "./pages/Product";
 import CartPage from "./pages/CartPage";
 import LoginPage from "./pages/LoginPage";
 import Footer from "./components/Footer/Footer";
+import men_banner from './assets/banner_mens.png';
+import women_banner from './assets/banner_women.png';
+import kids_banner from './assets/banner_kids.png'
 
 function App() {
   return (
@@ -15,9 +18,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<ShopPage />} />
-          <Route path="/mens" element={<CategoryPage category="men" />} />
-          <Route path="/womens" element={<CategoryPage category="women" />} />
-          <Route path="/kids" element={<CategoryPage category="kid" />} />
+          <Route path="/mens" element={<CategoryPage banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<CategoryPage banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<CategoryPage banner={kids_banner} category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,10 +1,42 @@
 import React, { useContext } from "react";
 import "./styles/CategoryPage.scss";
 import { ShopContext } from "../context/ShopContext";
+import dropdown_icon from "../assets/dropdown_icon.png";
+import Item from "../components/Item/Item";
 
 const CategoryPage = (props) => {
   const { all_product } = useContext(ShopContext);
-  return <div className="category-page">CategoryPage</div>;
+  return (
+    <div className="category">
+      <img className="category-banner" src={props.banner} alt="banner" />
+      <div className="category-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="category-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="category-products">
+        {all_product.map((item, index) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={index}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryPage;
